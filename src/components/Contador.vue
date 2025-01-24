fd2ef50461e3cee20c7b0db1f5706441dad7ada5
Jeremy <ballesterosramirez0705@gmail.com>
2025-01-24 13:36:33 -0500

    Componente contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,39 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  
+  <h1>
+    hola mundo
+  </h1>
+  <p>{{ count }}</p>
+
+  <button @click="increment">
+    Incrementar
+  </button>
+  <button @click="decrement">
+    Disminuir
+  </button>
+
+</template>
+
+<script setup>
+
+  import { ref } from 'vue';
+  const count = ref(0);
+
+  const increment = () => {
+    count.value++;
+  }
+  const decrement = () => {
+    count.value--;
+  }
+
+</script>
+
+<style scoped>
+
+  p {
+    color: red;
+  }
+  
+
+</style>
